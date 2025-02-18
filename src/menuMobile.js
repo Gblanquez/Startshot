ad2534e4a8ad24161c8ee9853f1b2270903c7836
Gblanquez <hellogabrielblanquez@gmail.com>
2025-02-18 21:56:44 +0800

    new mob

--- src/menuMobile.js
+++ src/menuMobile.js
@@ -101,8 +101,8 @@ export function initMobileMenu() {
       { opacity: 0 },
       { 
         opacity: 1,
-        duration: 0.5,
-        stagger: { each: 0.01, from: "random" },
+        duration: 0.8,
+        stagger: { each: 0.02, from: "random" },
         ease: "power2.out"
       },
       '<' // Align start
